9af15fb59a8ce6ded00607a273ab6f5bde168009
Максим <bogdanoff.96@yandex.ru>
2024-05-31 03:31:37 +1100

    Initial commit

---
+++ src/test/java/ru/skypro/homework28/Homework28ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
